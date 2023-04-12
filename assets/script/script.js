b84428b400487f61089fa33e71043b883697de4a
Yamini VK <yamini@Ashishs-MacBook-Pro.local>
2023-04-11 18:01:19 -0700

    updated the html and script for img tag

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,10 +2,11 @@ var bookDescription = document.querySelector("#bookDescription");
 var bookPhoto = document.querySelector("#bookPhoto");
 var authorName = document.querySelector("#authorName");
 var Title = document.querySelector("#bookTitle");
+var img = document.querySelector("#img");
+var advice = document.querySelector('#advice')
 var apiKey = "AIzaSyARQ1qCRDIdDsr2uR4uXZZnybC2lbkOA8w";
 function getApi() {
        var input = document.querySelector(".input");
-       console.log(input.value);
        // collects the data from the user
        var data = input.value; 
 
@@ -17,18 +18,16 @@ function getApi() {
         return response.json();
      })
      .then(function (data) {
-         console.log(data)
-        var books = data.items;
-        for (var i = 0; i < books.length; i++) {
-          Title.textContent ="The title of the Book is: " + books[i].volumeInfo.title;
-          authorName.textContent ="This book is written by: " + books[i].volumeInfo.authors;
-          bookDescription.textContent ="About the Book: " + books[i].volumeInfo.description;
+        var book = data.items[0];
+        Title.textContent ="The title of the Book is: " + book.volumeInfo.title;
+          authorName.textContent ="This book is written by: " + book.volumeInfo.authors;
+          bookDescription.textContent ="About the Book: " + book.volumeInfo.description;
           // have to make this url into a image and display
-          bookPhoto.textContent ="Book image is: " + books[i].volumeInfo.imageLinks.thumbnail;
-          }
+          bookPhoto.textContent ="Book image is: " + book.volumeInfo.imageLinks.thumbnail;
+          console.log(book.volumeInfo.imageLinks.thumbnail);
+          img.src = book.volumeInfo.imageLinks.thumbnail;   
   });
   }
   
   genNow.addEventListener('click', getApi);
-  
   
